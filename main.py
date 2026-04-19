5cd58e714e2377e98dfea93595787bce02fdf93a
taabb <tabdulmalikov@bk.ru>
2026-04-20 00:17:11 +0300

    Изменил main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -6,6 +7,5 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
